e5fd61a97c54af2d27227809b0b722640df379fd
sinha97 <vk9633698@gmail.com>
2019-08-31 16:33:22 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { User } from './User';
+import { CustomMap } from './CustomMap';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
